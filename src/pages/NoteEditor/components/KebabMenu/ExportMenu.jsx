2a3a9aabd20f9697cd349eec59f768749cdbffc2
dhdld <btj5420@naver.com>
2024-08-04 13:13:50 +0900

    Revert "feat: 공유하기 버튼 클릭 이벤트 추가"
    This reverts commit 6a5b538bcfd6b85de78185e295bf8feb7706b361.

--- src/pages/NoteEditor/components/KebabMenu/ExportMenu.jsx
+++ src/pages/NoteEditor/components/KebabMenu/ExportMenu.jsx
@@ -102,7 +102,7 @@ const ExportIcon = styled.div`
 // eslint-disable-next-line react/display-name
 const ExportMenu = forwardRef(({ onExportPDF, onExportCSV }, ref) => {
   const [selectedOption, setSelectedOption] = useState(null);
-  const [isCSVable, setIsCSVable] = useState(true);
+  const [isCSVable, setIsCSVable] = useState(false);
 
   const handleExportClick = () => {
     if (selectedOption === 'pdf') {
