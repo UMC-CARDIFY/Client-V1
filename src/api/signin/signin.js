369a21910cbe9324a47175e2fc2f8877ac38444b
daahyunk <ekgusdl0408@naver.com>
2024-08-07 20:00:45 +0900

    feat: 로그인 페이지 api 연동 완료

--- src/api/signin/signin.js
+++ src/api/signin/signin.js
@@ -3,13 +3,15 @@ import axios from 'axios';
 import config from '../config'; // config import 추가
 
 export const login = async (email, password) => {
+  const data = {
+    email: email,
+    password: password,
+  };
   try {
-    const response = await axios.post(`${config.apiBaseUrl}/users/login`, {
-      email,
-      password
-    });
+    const response = await axios.post(`${config.apiBaseUrl}/users/login`, data);
     return response.data;
   } catch (error) {
-    throw new Error(error.response?.data || '입력하신 이메일 또는 비밀번호가 잘못 되었습니다. 이메일과 비밀번호를 정확히 입력해 주세요.');
+    const errorMessage = error.response?.data?.message || '입력하신 이메일 또는 비밀번호가 잘못 되었습니다. 이메일과 비밀번호를 정확히 입력해 주세요.';
+    throw new Error(errorMessage);
   }
 };
