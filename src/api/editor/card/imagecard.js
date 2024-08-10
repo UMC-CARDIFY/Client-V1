59ad224464586a3351d00ed84bfdab913e6be947
dhdld <btj5420@naver.com>
2024-08-11 01:17:40 +0900

    feat: 가림판 카드 저장 후 불러오기 기능 구현중...

--- src/api/editor/card/imagecard.js
+++ src/api/editor/card/imagecard.js
@@ -18,3 +18,19 @@ export const uploadImageCard = async (imageFile, imageCard) => {
         console.error('Error:', error);
     }
 };
+
+export const getImageCard = async (cardId) => {
+    try {
+        const response = await axios.get(`${config.apiBaseUrl}/cards/view/${cardId}/Image`, {
+            headers: {
+                Authorization: 'eyJhbGciOiJIUzI1NiJ9.eyJ1c2VySWQiOjEsInR5cGUiOiJBY2Nlc3MiLCJzdWIiOiJBY2Nlc3NUb2tlbiIsImlhdCI6MTcyMzI3MDYxNiwiZXhwIjoxNzIzMjc0MjE2fQ.d33RXBtk9JCJqPgB6DCA-jgNypclLHsIBJJ0qvwitmw',
+                'Content-Type': 'multipart/form-data',
+            },
+        });
+        console.log('Success:', response.data);
+        return response.data;
+    }
+    catch (error) {
+        console.error('Error:', error);
+    }
+};
--- src/pages/NoteEditor/components/Cards/ImageCard.jsx
+++ src/pages/NoteEditor/components/Cards/ImageCard.jsx
@@ -1,7 +1,7 @@
 import { useRef, useState, useEffect } from 'react';
 import styled from 'styled-components';
 import imageIcon from '../../../../assets/images.png';
-import { uploadImageCard } from '../../../../api/editor/card/imagecard';
+import { getImageCard, uploadImageCard } from '../../../../api/editor/card/imagecard';
 
 const CardContainer = styled.div`
   width: 100%;
@@ -29,10 +29,11 @@ const Text = styled.p`
 `;
 
 const Canvas = styled.canvas`
-  border: 1px solid #CACACA;
   display: block;
-  width: 100%; /* CSS 크기 */
-  height: auto; /* CSS 크기 */
+  max-width: 65rem;
+  max-height: 40.5rem;
+  width: 100%;
+  height: auto;
 `;
 
 const ModalOverlay = styled.div`
@@ -41,7 +42,7 @@ const ModalOverlay = styled.div`
   left: 0;
   right: 0;
   bottom: 0;
-  background: rgba(0, 0, 0, 0.5);
+  background: rgba(0, 0, 0, 0.8); /* 모달 전체 화면을 어둡게 */
   display: flex;
   justify-content: center;
   align-items: center;
@@ -49,20 +50,90 @@ const ModalOverlay = styled.div`
 `;
 
 const ModalContent = styled.div`
-  background: white;
-  width: 65rem;
-  min-width: 40rem;
-  padding: 2rem;
-  z-index: 1001;
   position: relative;
+  max-width: 65rem;
+  max-height: 40.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: none; /* 배경을 없앰 */
+`;
+
+const BackButton = styled.button`
+  position: absolute;
+  top: 0.75rem;
+  left: 1rem;
+  cursor: pointer;
+  display: flex;
+  width: 2.5rem;
+  height: 2.5rem;
+  padding: 0.6875rem 0.625rem;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 0.5rem;
+  flex-shrink: 0;
+  border-radius: 0.25rem;
+  border: none;
+  background: var(--Grays-Gray8, #F4F4F4);
 `;
 
 const CloseButton = styled.button`
-  margin-top: 1rem;
+  position: absolute;
+  bottom: 0.75rem;
+  right: 5rem;
+  border: none;
+  cursor: pointer;
+  display: inline-flex;
+  padding: 0.4375rem 0.875rem 0.375rem 0.875rem;
+  justify-content: center;
+  align-items: center;
+  border-radius: 0.25rem;
+  background: var(--Grays-Gray8, #F4F4F4);
+  color: var(--Grays-Gray2, #767676);
+  font-family: Pretendard;
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
 `;
 
 const SubmitButton = styled.button`
-  margin-top: 1rem;
+  position: absolute;
+  bottom: 0.75rem;
+  right: 1rem;
+  border: none;
+  cursor: pointer;
+  display: inline-flex;
+  padding: 0.4375rem 0.875rem 0.375rem 0.875rem;
+  justify-content: center;
+  align-items: center;
+  border-radius: 0.25rem;
+  background: var(--Grays-Gray8, #F4F4F4);
+  color: var(--Grays-Black, #1A1A1A);
+  font-family: Pretendard;
+  font-size: 1rem;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+
+const ImageCardContainer = styled.div`
+  position: relative;
+flex-shrink: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const ImageCardImage = styled.img`
+  pointer-events: none; /* 이미지 수정 불가 */
+`;
+
+const Rectangle = styled.div`
+  position: absolute;
+  background-color: #6A9CFC;
+  z-index: 10;
 `;
 
 const ImageCard = () => {
@@ -75,6 +146,8 @@ const ImageCard = () => {
   const [isDragging, setIsDragging] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [imageFile, setImageFile] = useState(null);
+  const [isCreated, setIsCreated] = useState(false);
+  const [isLoaded, setIsLoaded] = useState(false); // 이미지 불러오기 상태 추가
 
   const handleCardClick = () => {
     fileInputRef.current.click();
@@ -82,8 +155,8 @@ const ImageCard = () => {
 
   const handleModalClose = () => {
     setIsModalOpen(false);
-    setImage(null);
-    setRectangles([]);
+    //setRectangles([]);
+    setIsLoaded(false); // 모달 닫을 때 상태 초기화
   };
 
   const handleFileChange = (event) => {
@@ -150,6 +223,13 @@ const ImageCard = () => {
       )
     );
   };
+
+  // 마지막으로 그린 사각형 삭제
+  const handleUndo = () => {
+    setRectangles((prevRectangles) => prevRectangles.slice(0, -1));
+  };
+
+  // 이미지 카드 저장하기
   const handleSubmit = async () => {
     if (!imageFile || rectangles.length === 0) return;
 
@@ -164,18 +244,10 @@ const ImageCard = () => {
       })),
     };
 
-    const formData = new FormData();
-    formData.append('image', imageFile);
-    formData.append('imageCard', JSON.stringify(imageCard));
-
-    console.log('FormData to be sent:');
-    formData.forEach((value, key) => {
-      console.log(key, value);
-    });
-
     try {
       const upload = await uploadImageCard(imageFile, imageCard);
       handleModalClose();
+      setIsCreated(true);
       console.log(upload);
     }
     catch (error) {
@@ -183,11 +255,35 @@ const ImageCard = () => {
     }
   };
 
+  // 이미지 카드 불러오기
+  const getImage = async (cardId) => {
+    try {
+      const imageCard = await getImageCard(cardId);
+      console.log(imageCard);
+      let img = new Image();
+      img.src = imageCard.imgUrl;
+      img.onload = () => {
+        setImage(img);
+        setRectangles(imageCard.overlays.map(overlay => ({
+          x: overlay.positionOfX,
+          y: overlay.positionOfY,
+          width: overlay.width,
+          height: overlay.height,
+        })));
+        setIsModalOpen(true);
+        setIsLoaded(true); // 이미지를 불러올 때 상태 설정
+      };
+    }
+    catch (error) {
+      alert('이미지 카드 불러오기에 실패했습니다.');
+    }
+  }
+
   useEffect(() => {
     if (isModalOpen && canvasRef.current && image) {
       const canvas = canvasRef.current;
-      const maxCanvasWidth = 950; // 원하는 최대 너비
-      const maxCanvasHeight = 700; // 원하는 최대 높이
+      const maxCanvasWidth = 1040; // 원하는 최대 너비
+      const maxCanvasHeight = 720; // 원하는 최대 높이
       let canvasWidth = image.width;
       let canvasHeight = image.height;
 
@@ -220,11 +316,11 @@ const ImageCard = () => {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
         rectangles.forEach((rect) => {
-          ctx.fillStyle = '#1062FE';
+          ctx.fillStyle = '#6A9CFC';
           ctx.fillRect(rect.x, rect.y, rect.width, rect.height);
         });
         if (newRect) {
-          ctx.fillStyle = '#1062FE';
+          ctx.fillStyle = '#6A9CFC';
           ctx.fillRect(newRect.x, newRect.y, newRect.width, newRect.height);
         }
       };
@@ -235,17 +331,37 @@ const ImageCard = () => {
 
   return (
     <>
-      <CardContainer onClick={handleCardClick}>
-        <ImageIcon src={imageIcon} alt="Icon" />
-        <Text>이미지 추가</Text>
-      </CardContainer>
-      <input
-        type="file"
-        ref={fileInputRef}
-        style={{ display: 'none' }}
-        accept="image/*"
-        onChange={handleFileChange}
-      />
+      <button onClick={() => getImage(12)}>이미지 카드 불러오기(test)</button>
+      {isCreated && image ? (
+        <ImageCardContainer>
+          <ImageCardImage src={image.src} alt="Image" />
+          {rectangles.map((rect, index) => (
+            <Rectangle
+              key={index}
+              style={{
+                left: `${rect.x}px`,
+                top: `${rect.y}px`,
+                width: `${rect.width}px`,
+                height: `${rect.height}px`,
+              }}
+            />
+          ))}
+        </ImageCardContainer>
+      ) : (
+        <>
+          <CardContainer onClick={handleCardClick}>
+            <ImageIcon src={imageIcon} alt="Icon" />
+            <Text>이미지 추가</Text>
+          </CardContainer>
+          <input
+            type="file"
+            ref={fileInputRef}
+            style={{ display: 'none' }}
+            accept="image/*"
+            onChange={handleFileChange}
+          />
+        </>
+      )}
       {isModalOpen && (
         <ModalOverlay>
           <ModalContent>
@@ -256,7 +372,21 @@ const ImageCard = () => {
               onMouseUp={handleMouseUp}
               onClick={handleRectangleClick}
             />
-            <CloseButton onClick={handleModalClose}>닫기</CloseButton>
+            <BackButton onClick={handleUndo}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="20"
+                height="16"
+                viewBox="0 0 20 16"
+                fill="none"
+              >
+                <path
+                  d="M0.821759 6.18334L0.410298 6.59218C0.183934 6.36662 0.183934 6.00006 0.410298 5.77451L0.821759 6.18334ZM20 15.4226C20 15.5757 19.9388 15.7226 19.8298 15.8309C19.7208 15.9392 19.573 16 19.4188 16C19.2647 16 19.1169 15.9392 19.0079 15.8309C18.8989 15.7226 18.8377 15.5757 18.8377 15.4226H20ZM6.63071 11.9605C6.4045 12.1852 6.03927 12.1852 5.81306 11.9605L0.410298 6.59218L1.23322 5.77451L6.63071 11.1376C6.85898 11.3644 6.85898 11.7337 6.63071 11.9605ZM0.410298 5.77451L5.81306 0.406219C6.03927 0.181451 6.4045 0.181451 6.63071 0.406218C6.85898 0.63303 6.85898 1.00231 6.63071 1.22912L1.23322 6.59218L0.410298 5.77451ZM0.821759 5.60589H12.4449V6.76079H0.821759V5.60589ZM20 13.1127V15.4226H18.8377V13.1127H20ZM12.4449 5.60589C14.4487 5.60589 16.3703 6.39679 17.7872 7.8046C19.204 9.21241 20 11.1218 20 13.1127H18.8377C18.8377 11.4281 18.1642 9.81246 16.9653 8.62124C15.7664 7.43001 14.1404 6.76079 12.4449 6.76079V5.60589Z"
+                  fill="#1A1A1A"
+                />
+              </svg>
+            </BackButton>
+            <CloseButton onClick={handleModalClose}>취소</CloseButton>
             <SubmitButton onClick={handleSubmit}>저장</SubmitButton>
           </ModalContent>
         </ModalOverlay>
