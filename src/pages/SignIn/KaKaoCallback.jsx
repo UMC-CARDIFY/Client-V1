559c794d9235d23b73c5db210dd64bbe64f9f1bf
daahyunk <ekgusdl0408@naver.com>
2024-08-12 20:38:16 +0900

    feat: 카카오 액세스 토큰 로컬 스토리지로 가져오는 기능 추가

--- src/pages/SignIn/KaKaoCallback.jsx
+++ src/pages/SignIn/KaKaoCallback.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import config from '../../api/config'; 
+import config from '../../api/config';
 
 const KakaoCallback = () => {
   const navigate = useNavigate();
@@ -13,10 +13,15 @@ const KakaoCallback = () => {
 
       if (code) {
         try {
-          // 인가 코드를 백엔드의 /api/v1/auth/kakao 엔드포인트로 전달
           const response = await axios.post(`${config.apiBaseUrl}/auth/kakao`, { code });
-          
-          console.log(response.data); // 백엔드 응답 데이터를 로그로 출력
+
+          // 확인용 로그 출력
+          console.log("응답 받은 데이터:", response.data);
+
+          // 받아온 액세스 토큰을 로컬 스토리지에 저장
+          localStorage.setItem('accessToken', response.data.accessToken);
+
+          // 로그인 성공 후 대시보드로 이동
           alert('카카오 로그인 성공');
           navigate('/dashboard');
         } catch (error) {
