891b8ad3c500d4e13ff259015bfdf63481478503
hyesngy <tell280210@gmail.com>
2024-08-15 22:46:23 +0900

    feat: 단어/빈칸/멀티카드 삭제버튼 추가

--- src/pages/NoteEditor/components/Editor/setup/wordcardView.js
+++ src/pages/NoteEditor/components/Editor/setup/wordcardView.js
@@ -28,16 +28,21 @@ class WordCardView {
     // 컨테이너 호버 시 이벤트 처리
     this.dom.addEventListener('mouseover', () => {
       this.previewButton.style.display = 'inline-block';
+      this.deleteButton.style.display = 'inline-block';
     });
     this.dom.addEventListener('mouseout', () => {
       this.previewButton.style.display = 'none';
+      this.deleteButton.style.display = 'none';
     });
 
     // 미리보기 버튼 추가
     this.previewButton = document.createElement('button');
     this.previewButton.style.position = 'absolute';
-    this.previewButton.style.top = '0%';
-    this.previewButton.style.right = '0.25rem';
+    this.previewButton.style.top = '-0.75rem';
+    this.previewButton.style.right = '2.5rem';
+    this.previewButton.style.width = '1.6875rem';
+    this.previewButton.style.height = '1.6875rem';
+    this.previewButton.style.padding = '0';
     this.previewButton.style.background = 'none';
     this.previewButton.style.border = 'none';
     this.previewButton.style.cursor = 'pointer';
@@ -56,6 +61,30 @@ class WordCardView {
       }
     });
     this.dom.appendChild(this.previewButton); // 미리보기 버튼을 dom에 추가
+
+    // 삭제 버튼
+    this.deleteButton = document.createElement('button');
+    this.deleteButton.style.position = 'absolute';
+    this.deleteButton.style.top = '-1.125rem';
+    this.deleteButton.style.right = '0.75rem';
+    this.deleteButton.style.width = '1.6875rem';
+    this.deleteButton.style.height = '1.6875rem';
+    this.deleteButton.style.marginTop = '0'
+    this.deleteButton.style.padding = '0'; 
+    this.deleteButton.style.background = 'none';
+    this.deleteButton.style.border = 'none';
+    this.deleteButton.style.cursor = 'pointer';
+    this.deleteButton.style.display = 'none';
+    this.deleteButton.innerHTML = `
+    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 30 30" fill="none">
+        <path fill-rule="evenodd" clip-rule="evenodd" d="M13.621 28.5801C13.3007 28.9233 12.7628 28.9418 12.4196 28.6215C12.0764 28.3012 12.0579 27.7633 12.3782 27.4201L18.8369 20.5001L12.3782 13.5801C12.0579 13.2369 12.0764 12.699 12.4196 12.3787C12.7628 12.0584 13.3007 12.0769 13.621 12.4201L19.9996 19.2543L26.3782 12.4201C26.6985 12.0769 27.2364 12.0584 27.5796 12.3787C27.9228 12.699 27.9413 13.2369 27.621 13.5801L21.1623 20.5001L27.621 27.4201C27.9413 27.7633 27.9228 28.3012 27.5796 28.6215C27.2364 28.9418 26.6985 28.9233 26.3782 28.5801L19.9996 21.7458L13.621 28.5801Z" fill="#B1B1B1"/>
+    </svg>
+  `;
+  
+    this.deleteButton.addEventListener('click', () => {
+        this.deleteCard();
+    });
+    this.dom.appendChild(this.deleteButton);
 
     // 카드 question(앞면) div
     this.questionDiv = document.createElement('div');
@@ -140,6 +169,11 @@ class WordCardView {
     //attrs 값 설정
     this.questionDiv.addEventListener('blur', this.updateAttrs.bind(this));
     this.answerDiv.addEventListener('blur', this.updateAttrs.bind(this));
+  }
+  
+  deleteCard() {
+    const transaction = this.view.state.tr.delete(this.getPos(), this.getPos() + this.node.nodeSize);
+    this.view.dispatch(transaction);
   }
 
   updateAttrs() {
