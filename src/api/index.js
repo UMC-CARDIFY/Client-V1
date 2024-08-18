81526ef35eb066eb3e2887d4ad29e2ecd1a1adda
daahyunk <ekgusdl0408@naver.com>
2024-08-18 16:56:34 +0900

    style: 카드 여러장 디자인 추가

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://3.37.13.40:8080/api/v1',
+  baseURL: 'http://localhost:8080/api/v1',
   timeout: 5000,
   withCredentials: true,
 });
