53e2e9f34b8bf78f41ce48e98e58068600ed2648
daahyunk <ekgusdl0408@naver.com>
2024-08-19 16:52:31 +0900

    chore: 설정 파일 .env로 API URL과 클라이언트 ID 관리

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://3.37.13.40:8080/api/v1',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   timeout: 5000,
   withCredentials: true,
 });
