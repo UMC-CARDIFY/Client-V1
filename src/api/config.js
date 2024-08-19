76cc8911124476134d90387775f016cf79be17d7
Dahyun <124647898+daahyunk@users.noreply.github.com>
2024-08-19 16:09:15 +0900

    chore: 카카오 redirect uri 배포 uri로 수정

--- src/api/config.js
+++ src/api/config.js
@@ -4,7 +4,7 @@ const config = {
   kakao: {
     clientId: '7150f2c7d3abb6ed45289bb918a0cd63',
     // redirectUri: 'http://3.37.13.40:8080/oauth2/callback/kakao',
-    redirectUri: 'http://localhost:5173/oauth2/callback/kakao',
+    redirectUri: 'http://43.201.167.247/oauth2/callback/kakao',
   }
 };
 
