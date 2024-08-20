ea2f75a5135f8f70a8bb6dc8183407a6cb9fa3f7
daahyunk <ekgusdl0408@naver.com>
2024-08-21 03:12:09 +0900

    fix: 충돌 해결

--- src/api/config.js
+++ src/api/config.js
@@ -1,16 +1,8 @@
 const config = {
-<<<<<<< HEAD
   apiBaseUrl: import.meta.env.VITE_API_BASE_URL,
   kakao: {
     clientId: import.meta.env.VITE_KAKAO_CLIENT_ID,
     redirectUri: import.meta.env.VITE_KAKAO_REDIRECT_URI,
-=======
-  // 로컬 개발 환경
-  apiBaseUrl: 'http://43.201.167.247:8080/api/v1',
-  kakao: {
-    clientId: '7150f2c7d3abb6ed45289bb918a0cd63',
-    redirectUri: 'http://localhost:5173/oauth2/callback/kakao',
->>>>>>> e840207565724cc8b22b55406b1841e018cbd72e
   }
 };
 
--- src/api/index.js
+++ src/api/index.js
@@ -1,11 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-<<<<<<< HEAD
   baseURL: import.meta.env.VITE_API_BASE_URL,
-=======
-  baseURL: 'http://43.201.167.247:8080/api/v1',
->>>>>>> e840207565724cc8b22b55406b1841e018cbd72e
   timeout: 5000,
   withCredentials: true,
 });
