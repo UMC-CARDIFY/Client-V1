9f6b480a56df1a503d486c890e00fe35ed39c071
hyesngy <tell280210@gmail.com>
2024-08-21 18:54:22 +0900

    refactor: 카드 미리보기 모달 style 리팩토링

---
+++ src/pages/NoteEditor/components/Cards/PreviewModal/style/CardPreviewModalStyles.jsx
@@ -0,0 +1,84 @@
+import styled from 'styled-components';
+
+export const Overlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: var(--Main-Overlay, rgba(0, 0, 0, 0.30));
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 999;
+`;
+
+export const ModalContainer = styled.div`
+  width: 55.625rem;
+  height: 34.25rem;
+  flex-shrink: 0;
+  border-radius: 0.75rem;
+  background: var(--Grays-White, #FFF);
+  box-shadow: 0px 4px 26px 0px rgba(0, 0, 0, 0.02), 0px 10px 60px 0px rgba(0, 74, 162, 0.03);
+  display: flex;
+  flex-direction: column;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: 1000;
+  padding: 2.5rem 5rem;
+`;
+
+export const ModalHeader = styled.div`
+  flex: 0 0 auto;
+  font-weight: bold;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const ModalContent = styled.div`
+  flex: 1 1 auto;
+  overflow-y: auto;
+
+  font-family: Pretendard;
+  font-size: 1.25rem;
+  font-style: normal;
+  font-weight: 300;
+  line-height: normal;
+`;
+
+export const PreviewTitle = styled.p`
+  color: var(--Main-Primary, #0F62FE);
+  font-family: Pretendard;
+  font-size: 0.9375rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+`;
+
+export const CloseButton = styled.button`
+  width: 2.5rem;
+  height: 2.5rem;
+  flex-shrink: 0;
+  background: none;
+  border: none;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+
+  svg {
+    width: 100%;
+    height: 100%;
+  }
+`;
+
+export const PreviewIcon = styled.div`
+  display: flex;
+  width: 1.6875rem;
+  height: 1.6875rem;
+  justify-content: center;
+  align-items: center;
+  margin-right: 0.5rem;
+`;
