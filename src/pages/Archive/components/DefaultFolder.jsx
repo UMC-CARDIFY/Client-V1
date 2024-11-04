ddb4a8047034dfc2b4f138ecafb225b4c3bd05f9
dhdld <btj5420@naver.com>
2024-11-04 15:11:31 +0900

    feat: 폴더 ui 구현 및 정렬, 필터링 버튼 추가

--- src/pages/Archive/components/DefaultFolder.jsx
+++ src/pages/Archive/components/DefaultFolder.jsx
@@ -2,6 +2,9 @@ import styled from "styled-components";
 import addFolderDefault from '../../../assets/addFolderDefault.png';
 import addFolderHover from '../../../assets/addFolderHover.png';
 import addFolderClick from '../../../assets/addFolderClick.png';
+import folderStar from "../../../assets/folderStar.svg";
+import kebab from "../../../assets/kebab.svg";
+import folder from "../../../assets/folderBig.svg";
 
 const DefaultFolder = () => {
 
@@ -10,18 +13,33 @@ const DefaultFolder = () => {
             <Name>
                 <p>name의 아카이브</p>
             </Name>
-            <SortDiv>
-                <p>정렬</p>
-            </SortDiv>
+            <SortAndFilterDiv>
+                <Sort>
+                최근 수정일 순
+                </Sort>
+                <Filtering>
+                필터링
+                </Filtering>
+            </SortAndFilterDiv>
             <FolderList>
                 <AddFolder
                 >
                     <img src={addFolderDefault} alt="Add Folder Icon" />
                 </AddFolder>
                 <Folder>
-                    {/* 다른 폴더 콘텐츠 */}
+                    <Star>
+                        <img src={folderStar}/>
+                    </Star>
+                    <FolderOptionBtn>
+                    <img src={kebab}/>
+                    </FolderOptionBtn>
+                    <FolderIcon>
+                    <img src={folder} />
+                    </FolderIcon>
+                    <FolderName>
+                        한국사
+                    </FolderName>
                 </Folder>
-                {/* 추가 Folder 컴포넌트들 */}
             </FolderList>
         </Container>
     );
@@ -47,8 +65,6 @@ const Name = styled.div`
   }
 `;
 
-const SortDiv = styled.div``;
-
 const FolderList = styled.div`
   display: flex;
   flex-wrap: wrap;
@@ -71,14 +87,17 @@ const AddFolder = styled.div`
     width: 100%;
     height: 100%;
     object-fit: contain;
-    transition: all 0.3s ease;
   }
 
-  /* hover 시 이미지를 addFolderHover로 변경 */
   &:hover img {
     content: url(${addFolderHover});
   }
+
+  &:active img {
+    content: url(${addFolderClick});
+  }
 `;
+
 
 const Folder = styled.div`
   width: 14.4375rem;
@@ -86,4 +105,80 @@ const Folder = styled.div`
   flex-shrink: 0;
   border-radius: 0.676rem;
   border: 2px solid #F5F5F5;
+  position: relative;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 0.94rem; /* FolderIcon과 FolderName 사이의 간격 */
 `;
+
+const Star = styled.div`
+position: absolute;
+left:1rem;
+top:1rem;
+width: 1.5rem;
+height: 1.5rem;
+flex-shrink: 0;
+`;
+
+const FolderOptionBtn = styled.div`
+position: absolute;
+right:1rem;
+top:1rem;
+width: 1.5rem;
+height: 1.5rem;
+flex-shrink: 0;
+`;
+
+const FolderIcon = styled.div`
+  width: 4.5rem;
+  height: 4.5rem;
+  flex-shrink: 0;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+`;
+
+const FolderName = styled.div`
+  overflow: hidden;
+  color: #1A1A1A;
+  text-align: center;
+  text-overflow: ellipsis;
+  font-size: 1.25rem;
+  font-weight: 600;
+`;
+
+const SortAndFilterDiv = styled.div`
+display:flex;
+gap:1rem;
+color: #1A1A1A;
+font-size: 1.125rem;
+font-weight: 600;
+margin-bottom:2rem;
+`
+
+const Sort = styled.div`
+    display: inline-flex;
+padding: 0.75rem 1.25rem;
+justify-content: center;
+align-items: center;
+gap: 1rem;
+border-radius: 0.75rem;
+background: #F5F5F5;
+cursor: pointer;
+  &:hover {
+    color: #1062FE;
+  }
+&:active {
+background: #E3E3E3;
+}
+`
+
+const Filtering  = styled(Sort)`
+gap: 0.5rem;
+`
